77d0e3f864edd353f2896fe5b1d4fd64207c2964
Zbr123 <syedzubairalam123@gmail.com>
2025-07-04 04:17:37 -0700

    newcicdd

--- cypress/support/pageObjects/loginPage.js
+++ cypress/support/pageObjects/loginPage.js
@@ -1,42 +1,42 @@
 class LoginPage {
-  enterEmail(useremail) {
-    cy.get('input[placeholder="Email"]', { timeout: 20000 }) // Primary attempt
-      .should('be.visible')
-      .clear()
-      .type(useremail)
-      .then($el => {
-        if ($el.length === 0) {
-          cy.log('Placeholder "Email" not found, trying type="email"');
-          cy.get('input[type="email"]', { timeout: 20000 }) // Fallback
-            .should('be.visible')
-            .clear()
-            .type(useremail);
-        }
-      });
-  }
-
-  enterPassword(password) {
-    cy.get('input[placeholder="Password"]', { timeout: 20000 })
-      .should('be.visible')
-      .clear()
-      .type(password)
-      .then($el => {
-        if ($el.length === 0) {
-          cy.log('Placeholder "Password" not found, trying type="password"');
-          cy.get('input[type="password"]', { timeout: 20000 })
-            .should('be.visible')
-            .clear()
-            .type(password);
-        }
-      });
-  }
   // enterEmail(useremail) {
-  //   cy.get('input[placeholder="Email"]').clear().type(useremail);
+  //   cy.get('input[placeholder="Email"]', { timeout: 20000 }) // Primary attempt
+  //     .should('be.visible')
+  //     .clear()
+  //     .type(useremail)
+  //     .then($el => {
+  //       if ($el.length === 0) {
+  //         cy.log('Placeholder "Email" not found, trying type="email"');
+  //         cy.get('input[type="email"]', { timeout: 20000 }) // Fallback
+  //           .should('be.visible')
+  //           .clear()
+  //           .type(useremail);
+  //       }
+  //     });
   // }
 
   // enterPassword(password) {
-  //   cy.get('input[placeholder="Password"]').clear().type(password);
+  //   cy.get('input[placeholder="Password"]', { timeout: 20000 })
+  //     .should('be.visible')
+  //     .clear()
+  //     .type(password)
+  //     .then($el => {
+  //       if ($el.length === 0) {
+  //         cy.log('Placeholder "Password" not found, trying type="password"');
+  //         cy.get('input[type="password"]', { timeout: 20000 })
+  //           .should('be.visible')
+  //           .clear()
+  //           .type(password);
+  //       }
+  //     });
   // }
+  enterEmail(useremail) {
+    cy.get('input[placeholder="Email"]').clear().type(useremail);
+  }
+
+  enterPassword(password) {
+    cy.get('input[placeholder="Password"]').clear().type(password);
+  }
 
   clickLogin() {
     cy.get('button[id="btnLogin"]').click();
