fdfd8b3219e79855afc0c87fdc7d2c600bdbdef7
Zbr123 <syedzubairalam123@gmail.com>
2025-07-04 01:59:20 -0700

    newcicd

--- cypress.config.js
+++ cypress.config.js
@@ -26,19 +26,16 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.feature",
     async setupNodeEvents(on, config) {
-      // Add Cucumber preprocessor plugin
       await addCucumberPreprocessorPlugin(on, config);
       on("file:preprocessor", createBundler({
         plugins: [createEsbuildPlugin(config)],
       }));
-      // Properly initialize cypress-file-upload
-      on('task', require('cypress-file-upload/lib/register')); // Use task to register the plugin
       return config;
     },
     env: {
       baseUrl: process.env.CYPRESS_baseUrl || "https://filmd-apps-dev-uks-app-app-01.azurewebsites.net",
-      validEmail: process.env.CYPRESS_validEmail || "zubair.a@yetiinc.com", // Match your secret
-      validPassword: process.env.CYPRESS_validPassword || "Vista123+" // Match your secret
+      validEmail: process.env.CYPRESS_validEmail || "zubair.a@yetiinc.com",
+      validPassword: process.env.CYPRESS_validPassword || "Vista123+"
     },
   },
 });
